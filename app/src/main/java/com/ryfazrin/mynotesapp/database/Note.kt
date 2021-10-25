238c7459a62b1f5c0c3d9a67bbeb3ede8517f11d
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-25 12:07:28 +0800

    Update Note.kt
    Buat data class Note dan tambahkan beberapa variabel seperti id, title, description, date

--- app/src/main/java/com/ryfazrin/mynotesapp/database/Note.kt
+++ app/src/main/java/com/ryfazrin/mynotesapp/database/Note.kt
@@ -1,4 +1,21 @@
 package com.ryfazrin.mynotesapp.database
 
-class Note {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+
+    @ColumnInfo(name = "title")
+    var title: String? = null,
+
+    @ColumnInfo(name = "description")
+    var description: String? = null,
+
+    @ColumnInfo(name = "date")
+    var date: String? = null
+)
