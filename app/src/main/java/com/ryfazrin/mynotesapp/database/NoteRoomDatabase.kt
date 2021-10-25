3ad2257f87508556290a2113743f8be76c581dff
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-25 12:14:41 +0800

    Update NoteRoomDatabase.kt
    Digunakan untuk menginisialisasi database dalam aplikasi. Tambahkan implementasi RoomDatabase, tambahkan annotation @Database dan ubah menjadi abstract class

--- app/src/main/java/com/ryfazrin/mynotesapp/database/NoteRoomDatabase.kt
+++ app/src/main/java/com/ryfazrin/mynotesapp/database/NoteRoomDatabase.kt
@@ -1,4 +1,9 @@
 package com.ryfazrin.mynotesapp.database
 
-class NoteRoomDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteRoomDatabase : RoomDatabase() {
+    
 }
