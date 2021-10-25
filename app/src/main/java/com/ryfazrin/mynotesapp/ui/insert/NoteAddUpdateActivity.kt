ebfbf50aabd3628db8cdbbf4f27bb650d904e1b3
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-25 12:29:57 +0800

    Create package main & insert for Activity
    Setelah menyiapkan repository dan database. Buatlah sebuah package baru dengan nama ui. Di dalamnya buat lagi 2 package baru dengan nama insert dan main.
    
    Setelah itu, pindahkan MainActivity ke package main. Ini untuk memudahkan memilah-milah fungsi dari masing-masing kelas. Setelah itu buat sebuah activity baru dengan nama NoteAddUpdateActivity

---
+++ app/src/main/java/com/ryfazrin/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.ryfazrin.mynotesapp.ui.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ryfazrin.mynotesapp.R
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
--- app/src/main/java/com/ryfazrin/mynotesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/mynotesapp/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ryfazrin.mynotesapp
+package com.ryfazrin.mynotesapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ryfazrin.mynotesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
