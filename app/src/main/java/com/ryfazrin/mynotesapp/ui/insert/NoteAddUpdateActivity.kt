6d58dca6698d5fcd75b2b94c7a36d71481f66076
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-25 14:52:22 +0800

    Update NoteAddUpdateActivity.kt
    Menginisialisasi view yang ada di layout-nya.

--- app/src/main/java/com/ryfazrin/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/ryfazrin/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
@@ -3,8 +3,19 @@ package com.ryfazrin.mynotesapp.ui.insert
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ryfazrin.mynotesapp.R
+import com.ryfazrin.mynotesapp.database.Note
 
 class NoteAddUpdateActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+
+    private var isEdit = false
+    private var note: Note? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_add_update)
