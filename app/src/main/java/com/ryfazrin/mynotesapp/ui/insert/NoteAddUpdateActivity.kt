648c037364f730d63687aa5da17675aa88944190
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-25 17:41:34 +0800

    Update NoteAddUpdateActivity.kt
    Kode untuk menghapus item dari database

--- app/src/main/java/com/ryfazrin/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
+++ app/src/main/java/com/ryfazrin/mynotesapp/ui/insert/NoteAddUpdateActivity.kt
@@ -2,7 +2,10 @@ package com.ryfazrin.mynotesapp.ui.insert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import com.ryfazrin.mynotesapp.R
 import com.ryfazrin.mynotesapp.database.Note
@@ -94,13 +97,65 @@ class NoteAddUpdateActivity : AppCompatActivity() {
         }
     }
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        if (isEdit) {
+            menuInflater.inflate(R.menu.menu_form, menu)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_delete -> showAlertDialog(ALERT_DIALOG_DELETE)
+            android.R.id.home -> showAlertDialog(ALERT_DIALOG_CLOSE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        showAlertDialog(ALERT_DIALOG_CLOSE)
+    }
+
+    private fun showAlertDialog(type: Int) {
+        val isDialogClose = type == ALERT_DIALOG_CLOSE
+        val dialogTitle: String
+        val dialogMessage: String
+        if (isDialogClose) {
+            dialogTitle = getString(R.string.cancel)
+            dialogMessage = getString(R.string.message_cancel)
+        } else {
+            dialogMessage = getString(R.string.message_delete)
+            dialogTitle = getString(R.string.delete)
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        with(alertDialogBuilder) {
+            setTitle(dialogTitle)
+            setMessage(dialogMessage)
+            setCancelable(false)
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                if (!isDialogClose) {
+                    noteAddUpdateViewModel.delete(note as Note)
+                    showToast(getString(R.string.deleted))
+                }
+
+                finish()
+            }
+            setNegativeButton(getString(R.string.no)) { dialog, _ ->
+                dialog.cancel()
+            }
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _activityNoteAddUpdateBinding = null
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
