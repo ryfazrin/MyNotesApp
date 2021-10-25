b4004e64f8a349bcf1f17984b4c88f905f9b852f
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-26 00:01:24 +0800

    Update MainActivity.kt
    Menginisialisasi RecyclerView dan NoteAdapter

--- app/src/main/java/com/ryfazrin/mynotesapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/mynotesapp/ui/main/MainActivity.kt
@@ -2,11 +2,32 @@ package com.ryfazrin.mynotesapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ryfazrin.mynotesapp.R
+import com.ryfazrin.mynotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        adapter = NoteAdapter()
+
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
     }
 }
